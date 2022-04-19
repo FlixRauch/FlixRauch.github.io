ea64915ba957135d865742780a1cbb1494230135
felix <felix.rauch@student.uibk.ac.at>
2022-04-19 12:43:25 +0200

    Einfügen von neuen Skriptbausteinen und iterieren durch Etappen

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,30 @@
 /* Neuseelandreise Skript */
 
-var map = L.map('map').setView([-43.443767, 170.173520], 11);
+let lat = -43.443767;
+let lng = 170.173520;
+let zoom = 11;
+let coords = [-39.13, 175.65];
+
+let popup = `
+    <h3>${ETAPPEN[0].titel} (Etappe ${ETAPPEN[0].nr})</h3>
+    <ul>
+        <li>geogr. Länge: ${ETAPPEN[0].lng}</li>
+        <li>geogr. Breite: ${ETAPPEN[0].lat}</li>
+        <li><a href="${ETAPPEN[0].wikipedia}">Link zur Wikipediaseite</a></li>
+        <li><a href="${ETAPPEN[0].github}">Link zur Etappenseite</a></li>
+    </ul>
+`;
+let map = L.map('map').setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-43.443767, 170.173520]).addTo(map)
-    .bindPopup('<h3>Franz-Josef-Gletscher</h3>')
+L.marker([lat, lng]).addTo(map)
+    .bindPopup(popup)
     .openPopup();
+
+for (let etappe of ETAPPEN) {
+    //console.log(etappe);
+    L.marker([etappe.lat, etappe.lng]).addTo(map);
+} 
