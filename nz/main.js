3a4fe5dfad0957db7a6a9c4f297e541ec5989a42
felix <felix.rauch@student.uibk.ac.at>
2022-04-28 15:41:58 +0200

    Unnötigen Code gelöscht

--- nz/main.js
+++ nz/main.js
@@ -1,28 +1,21 @@
 /* Neuseelandreise Skript */
 
-let lat = -43.443767;
-let lng = 170.173520;
-let zoom = 11;
-let coords = [-39.13, 175.65];
 
-let popup = `
-    <h3>${ETAPPEN[0].titel} (Etappe ${ETAPPEN[0].nr})</h3>
-    <ul>
-        <li>geogr. Länge: ${ETAPPEN[0].lng}</li>
-        <li>geogr. Breite: ${ETAPPEN[0].lat}</li>
-        <li><a href="${ETAPPEN[0].wikipedia}">Link zur Wikipediaseite</a></li>
-        <li><a href="${ETAPPEN[0].github}">Link zur Etappenseite</a></li>
-    </ul>
-`;
+let zoom = 11;
+let coords = [
+    ETAPPEN[10].lat,
+    ETAPPEN[10].lng
+];
+
+
+
 let map = L.map('map').setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([lat, lng]).addTo(map)
-    .bindPopup(popup)
-    .openPopup();
+
 
 for (let etappe of ETAPPEN) {
     //console.log(etappe);
