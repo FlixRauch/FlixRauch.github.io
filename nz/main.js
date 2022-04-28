2aeb38b99c070d56f87ced27f3e23b3a436f7ec5
felix <felix.rauch@student.uibk.ac.at>
2022-04-28 16:06:38 +0200

    NAvigation eigene Etappe markieren

--- nz/main.js
+++ nz/main.js
@@ -28,12 +28,14 @@ for (let etappe of ETAPPEN) {
       <li><a href="https://${etappe.github}.github.io/nz/">Link zur Etappenseite</a></li>
   </ul>
   `;
+    let navClass = "etappenLink"
     let mrk = L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(popup);
     if (etappe.nr == 9){
         mrk.openPopup();
+        navClass="etappenLink etappeAktuell"
     }
     // Etappennavigation erweitern
-    let link = `<a href="https://${etappe.github}.github.io/nz/" class="etappenLink" title="${etappe.titel}">${etappe.nr}</a>`;
+    let link = `<a href="https://${etappe.github}.github.io/nz/" class="${navClass}" title="${etappe.titel}">${etappe.nr}</a>`;
     document.querySelector("#navigation").innerHTML += link;
 }
 
